85994dd69b1a20959f5a05728ba2876c7cde9178
Mateusz Król <matek8952@gmail.com>
2020-06-30 13:02:16 +0200

    Folder renamed

---
+++ adminApp/server/models/meal.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const mealSchema = Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+        required: false,
+    },
+    restaurantId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Restaurant',
+        required: true,
+    },
+});
+
+module.exports = mongoose.model('Meal', mealSchema);
